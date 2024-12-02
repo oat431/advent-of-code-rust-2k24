1ca72debdd535c7b97ec7583ce2e962ad74b528a
oat431 <oat431@gmail.com>
2024-12-02 23:43:27 +0700

    🔨 add: day 1 part 2

--- src/bin/Day01.rs
+++ src/bin/Day01.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use anyhow::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -9,11 +10,9 @@ const DAY: &str = "01";
 const INPUT_FILE: &str = concatcp!("input/day", DAY, ".txt");
 fn main() -> Result<()> {
     start_day(DAY);
-    println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
         let input = reader.lines().flatten().collect::<Vec<String>>();
-        // input.iter().for_each(|line| println!("{}", line));
 
         let mut left: Vec<i32> = Vec::new();
         let mut right: Vec<i32> = Vec::new();
@@ -38,10 +37,50 @@ fn main() -> Result<()> {
         Ok(distances as usize)
     }
 
-    let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    let result = part1(input_file)?;
 
-    println!("Result = {}", result);
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let input = reader.lines().flatten().collect::<Vec<String>>();
+        let mut left: HashSet<i32> = HashSet::new();
+        let mut right: Vec<i32> = Vec::new();
+
+        for lines in input.iter() {
+            let line = lines;
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            let l : i32 = parts[0].parse::<i32>()?;
+            let r : i32 = parts[1].parse::<i32>()?;
+            left.insert(l);
+            right.push(r);
+        }
+
+        let mut similar_score: HashMap<i32, i32> = HashMap::new();
+        for &l in &left {
+            similar_score.insert(l, 0);
+        }
+
+        for &r in &right {
+            if let Some(score) = similar_score.get_mut(&r) {
+                *score += 1;
+            }
+        }
+
+        let sum: i32 = similar_score.iter().map(|(&key, &value)| key * value).sum();
+
+        Ok(sum as usize)
+    }
+
+
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result_p1 = part1(input_file)?;
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result_p2 = part2(input_file)?;
+
+    println!("=== Part 1 ===");
+    println!("Result = {}", result_p1);
+
+    println!("=== Part 2 ===");
+    println!("Result = {}", result_p2);
 
     Ok(())
 }
