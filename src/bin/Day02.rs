eed748ab44cbd004b3bdd15e234e401931a21ad3
oat431 <oat431@gmail.com>
2024-12-03 00:09:40 +0700

    add: day02 part 1 solution

---
+++ src/bin/Day02.rs
@@ -0,0 +1,56 @@
+use std::io::{BufRead, BufReader};
+use adv_code_2024::start_day;
+use const_format::concatcp;
+use anyhow::*;
+
+const DAY: &str = "02";
+const INPUT_FILE: &str = concatcp!("input/day", DAY, ".txt");
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        let input = reader.lines().flatten().collect::<Vec<String>>();
+        let mut safe_reports = 0;
+
+        for line in input {
+            let range: Vec<i32> = line
+                .split_whitespace()
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect();
+            if is_safe_report(&range) {
+                safe_reports += 1;
+            }
+        }
+        Ok(safe_reports)
+    }
+
+    fn is_safe_report(nums: &[i32]) -> bool {
+        is_increasing(nums) || is_decreasing(nums)
+    }
+
+    fn is_increasing(nums: &[i32]) -> bool {
+        nums.windows(2).all(|pair| is_fit_in_lesser_than_condition(pair[0], pair[1]))
+    }
+
+    fn is_decreasing(nums: &[i32]) -> bool {
+        nums.windows(2).all(|pair| is_fit_in_greater_than_condition(pair[0], pair[1]))
+    }
+
+    fn is_fit_in_lesser_than_condition(a: i32, b:i32) -> bool {
+        let diff = b - a;
+        matches!(diff, 1 | 2 | 3)
+    }
+
+    fn is_fit_in_greater_than_condition(a: i32, b:i32) -> bool {
+        let diff = a - b;
+        matches!(diff, 1 | 2 | 3)
+    }
+
+    let input_file = BufReader::new(std::fs::File::open(INPUT_FILE)?);
+    let result_p1 = part1(input_file)?;
+
+    println!("=== Part 1 ===");
+    println!("Result: {}", result_p1);
+
+    Ok(())
+}
